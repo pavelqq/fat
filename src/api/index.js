afd837833039d09838549920bdf2674300dd0a3a
alxnoob <sad12sad12sad12@gmail.com>
2021-07-10 12:14:01 +0500

    подготовка клиента к деплою, удалил бекенд, т.к. решил обратно перенести его в другой репозиторий

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
-export const url = "https://mernfatapptest.herokuapp.com/fat/api";
+export const url = "https://apifat.herokuapp.com/api";
 
 export const setHeaders = () => {
     const headers = {
