c24d1fcc6fe0c0efa613ed9dbc324c50a22e675c
alxnoob <sad12sad12sad12@gmail.com>
2021-07-10 20:52:41 +0500

    path

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.static('build'));
+app.use(express.static(path.join(__dirname, "build")));
 app.get('*', function (req, res) {
-    res.sendFile('index.html');
+    res.sendFile(path.join(__dirname, "./build/index.html"));
 });
 
 app.listen(port, () => {
